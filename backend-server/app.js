ea93aaa8279d7976eb972386e15bcc2c9b240e10
jsdysw <jsdysw@gmail.com>
2022-02-12 13:07:59 +0900

    build error handling middlewares

---
+++ backend-server/app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const morgan = require('morgan');
+const dotenv = require('dotenv');
+
+
+dotenv.config();
+
+
+const app = express();
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+
+app.use(express.json());
+
+
+app.use((req, res, next) => {
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
+});
+
+app.use((err, req, res, next) => {
+  res.json({
+    status: err.status || 500,
+    message: err.message
+ });
+});
+
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});

1520a2049fd36541d6d40d886ec3df0d73ae2111
jsdysw <jsdysw@gmail.com>
2022-02-12 16:47:11 +0900

    add sequelize model

--- backend-server/app.js
+++ backend-server/app.js
@@ -2,17 +2,29 @@ const express = require('express');
 const morgan = require('morgan');
 const dotenv = require('dotenv');
 
-
 dotenv.config();
+
+const { sequelize } = require('./models');
 
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+sequelize.sync({ force: false })
+  .then(() => {
+    console.log('데이터베이스 연결 성공');
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
 
 app.use(morgan('dev'));
-
 app.use(express.json());
 
+
+app.get('/',(req, res, next) => {
+  res.json("hi");
+});
 
 app.use((req, res, next) => {
   const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);

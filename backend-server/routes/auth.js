62b1a1478578f0fd21f3977465845307ef0d7101
jsdysw <jsdysw@gmail.com>
2022-02-14 21:59:55 +0900

    complete login router

--- backend-server/routes/auth.js
+++ backend-server/routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
 const router = express.Router();
@@ -28,26 +30,59 @@ router.post('/signup', async (req, res, next) => {
   }
 });
 
-// router.post('/login', (req, res, next) => {
-//   passport.authenticate('local', (authError, user, info) => {
-//     if (authError) {
-//       console.error(authError);
-//       return next(authError);
-//     }
-//     if (!user) {
-//       return res.redirect(`/?loginError=${info.message}`);
-//     }
-//     return req.login(user, (loginError) => {
-//       if (loginError) {
-//         console.error(loginError);
-//         return next(loginError);
-//       }
-//       return res.redirect('/');
-//     });
-//   })(req, res, next); // 미들웨어 내의 미들웨어에는 (req, res, next)를 붙입니다.
-// });
+router.post('/login', async (req, res, next) => {
+  const { email, password } = req.body;
+
+  // id, password match
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      const result = await bcrypt.compare(password, exUser.password);
+      if (!result) {
+        return res.status(401).json({
+          message: 'wrong password',
+        });
+      }
+    } else {
+      return res.status(401).json({
+        message: 'no such account',
+      });
+    }
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+
+  // post token
+  try {
+    const accesstoken = jwt.sign({
+      email: email,
+    }, process.env.JWT_SECRET, {
+      expiresIn: '30m', // 30 minute
+      issuer: 'admin',
+    });
+
+    const refreshtoken = jwt.sign({
+      email: email,
+    }, process.env.JWT_SECRET, {
+      expiresIn: '30d', // 30 days
+      issuer: 'admin',
+    });
+
+    return res.status(200).json({
+      message: 'login succeed',
+      accesstoken,
+      refreshtoken,
+    })
+
+  } catch (error) {
+    console.error(error);
+    return next(error);
+  }
+});
 
 router.get('/logout', (req, res) => {
+  // client shoud expire token
   res.status(200).json({message: 'logout'});
 });
 
